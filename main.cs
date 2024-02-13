155bf888d216569a250c03678c18e4393fc37800
JynxBot <158657006+JynxBot@users.noreply.github.com>
2024-02-13 03:33:03 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string 0.01 = "No Person is found";
+    string 0.05 = "No Person is found";
+    string 0.25 = "No Person is found";
+    string 1 = "Jose Rizal";
+    string 5 = "Emilio Aguinaldo";
+    string 10 = "Andres Bonifacio, Apolinario Mabini";
+    string 20 = "Manuel L. Quezon";
+    string 50 = "Sergio Osmena";
+    string 100 = "Manuel Roxas";
+    string 200 = "Diosdado Macapagal";
+    string 500 = "Benigno Sr. and Corazon Aquino";
+    string 1000 = "Jose Abad Santos, Vicente Lim, Josefa Llanes Escoda";
+
+    
+  }
+}
